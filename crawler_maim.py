d33020e8a7fecf0e950df8acb9e16da714fec890
vithal <vithalchoudhary4@gmail.com>
2024-08-03 18:12:42 +0530

    main

---
+++ crawler_maim.py
@@ -0,0 +1,18 @@
+
+import requests
+from bs4 import BeautifulSoup
+def flip_spider(max_page):
+    page=1
+    # while page <=max_page:
+    url='https://www.ebay.com/sch/i.html?_from=R40&_nkw=ppokemon+cards&_sacat=0&_pgn='+str(page)
+    # url = 'https://github.com/metaladmiral/'
+    source_code=requests.get(url)
+    plain_text=source_code.text
+    soup = BeautifulSoup(plain_text, 'html.parser')
+    for link in soup.findAll('a',{'class':'s-item__link'}):
+        # div = 'https://www.ebay.com'+link.get('div')
+        print(link.text,"\n")
+    page+=1     
+
+flip_spider(1)        
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+l.parser')
+    # #  for link in soup.findAll('a',{'class':'s-item__title'}):
+    # #     div = 'https://www.ebay.com'+link.get('div')
+    # #     print(div)
