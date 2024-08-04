8edb56d78d31bf4705e1e08fdbb9b0dde8cee6f3
vithal <vithalchoudhary4@gmail.com>
2024-08-04 14:53:30 +0530

    python

--- crawler_maim.py
+++ crawler_maim.py
@@ -1,16 +1,23 @@
+import mysql.connector
+conn=mysql.connector.connect(
+    host='localhost', password ='vithal1234',user='root',database='python_data'
+)
+if conn.is_connected():
+    print("Connected to MySQL Database")
+
+
+
+
 
 import requests
 from bs4 import BeautifulSoup
 def flip_spider(max_page):
     page=1
-    # while page <=max_page:
     url='https://www.ebay.com/sch/i.html?_from=R40&_nkw=ppokemon+cards&_sacat=0&_pgn='+str(page)
-    # url = 'https://github.com/metaladmiral/'
     source_code=requests.get(url)
     plain_text=source_code.text
     soup = BeautifulSoup(plain_text, 'html.parser')
     for link in soup.findAll('a',{'class':'s-item__link'}):
-        # div = 'https://www.ebay.com'+link.get('div')
         print(link.text,"\n")
     page+=1     
 
