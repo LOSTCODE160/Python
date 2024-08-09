c40ea817eaf958973c6b2d49d43ab2baf14352b5
vithal <vithalchoudhary4@gmail.com>
2024-08-09 23:18:59 +0530

    vithal

--- threading_tut3.py
+++ threading_tut3.py
@@ -1,12 +1,24 @@
-import threading
-import time
+# import threading
+# import time
  
+# class messanger(threading.Thread):
+#     def run(self):
+#        for _ in range(10):
+#            print(threading.current_thread().getName())
+#            time.sleep(5)
+# x=messanger(name="message is send")
+# y=messanger(name="message is receive")
+# x.start()
+# y.start()
+
+import threading
+import time 
 class messanger(threading.Thread):
     def run(self):
-       for _ in range(10):
-           print(threading.current_thread().getName())
-           time.sleep(5)
-x=messanger(name="message is send")
+        for _ in range(10):
+            print(threading.current_thread().getName())
+            time.sleep(5)
+x=messanger(name="mesasage is send")
 y=messanger(name="message is receive")
 x.start()
 y.start()
