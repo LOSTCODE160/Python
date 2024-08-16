040eb8d6dbe0b49c1fda0afcfe62f442eeee0164
vithal <vithalchoudhary4@gmail.com>
2024-08-16 17:02:59 +0530

    zip

---
+++ zip.py
@@ -0,0 +1,10 @@
+first_name=["vithal","shivarj","pragun"]
+last_name=["choudhary","garg","sharma"]
+
+names= zip(first_name,last_name)
+print(set(names)) #method one 
+
+# for name in names:
+#     print(name)
+
+    
