d3a8457ec61e0868ffee6aec6ac131f994d42410
vithal <vithalchoudhary4@gmail.com>
2024-08-20 19:52:32 +0530

    lamdba

--- lambda.py
+++ lambda.py
@@ -1,2 +1,6 @@
-add = lambda x, y: x + y
-print(add(5, 3))
+# add = lambda x, y: x + y
+# print(add(5, 3))
+
+numbers = [1, 2, 3, 4, 5]
+squared = list(map(lambda x: x**2, numbers))
+print(squared) 
